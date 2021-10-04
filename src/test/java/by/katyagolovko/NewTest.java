b1c5a517ffd00bce05fa4a2f3c209f43ee51dc64
ekaterinagolovko <katharinegolovko@gmail.com>
2021-10-04 21:13:52 +0300

    Added new Test

---
+++ src/test/java/by/katyagolovko/NewTest.java
@@ -0,0 +1,12 @@
+package by.katyagolovko;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+
+    @Test
+    public void newTest(){
+        Assertions.assertTrue(true);
+    }
+}
